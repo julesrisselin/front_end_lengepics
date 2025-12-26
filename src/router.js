88bb3ea22829fda3c817e0f69101b3b4ed6b306f
Jules <julesrisselin@gmail.com>
2025-12-26 14:00:39 +0100

    ajout page 2

--- src/router.js
+++ src/router.js
@@ -2,16 +2,26 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 import home from './Views/home.vue'
 import currentChallenge from './Views/currentChallenge.vue'
-import participations from './Views/participations.vue'
-import currentParticipations from './Views/currentParticipations.vue'
+import participations_infinite from './Views/participations_infinite.vue'
+import participation from './Views/participation.vue'
 import login from './Views/login.vue'
+import signin from './Views/signin.vue'
+import CurrentParticipations from './Views/currentParticipations.vue'
+import account from './Views/account.vue'
+import history from './Views/history.vue'
+import moderator from './Views/moderator.vue'
 
 const routes = [
     { path: '/', component: home },
     {path: '/currentChallenge', component: currentChallenge},
-    {path: '/participations', component: participations},
-    {path: '/currentParticipations', component: currentParticipations},
+    {path: '/participations', component: participations_infinite},
+    {path: '/participation', component: participation},
     {path: '/login', component: login},
+    {path: '/signin', component: signin},
+    {path: '/currentParticpations', component: CurrentParticipations},
+    {path: '/account', component: account},
+    {path: '/history', component: history},
+    {path: '/moderator', component: moderator}
 ]
 
 
