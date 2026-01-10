0159558edbd0d5037cbfb7938a21604dd9de7054
Jules <julesrisselin@gmail.com>
2026-01-10 13:25:17 +0100

    Début login

--- src/Views/login.vue
+++ src/Views/login.vue
@@ -2,7 +2,12 @@
 </script>
 
 <template>
-login
+<form action="http://localhost:3000/auth/login" enctype="multipart/form-data" method="post">
+            <div class="form-group">
+                <input type="text" class="email" placeholder="email" name="email">
+                <input type="text" class="password" placeholder="password" name="password">
+            </div>
+        </form>
 </template>
 
 <style scoped>
