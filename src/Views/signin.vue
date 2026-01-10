9e0dafbbf6fb8c78dc8f9f4effdd949109515c52
Jules <julesrisselin@gmail.com>
2026-01-10 13:38:00 +0100

    début sign in

--- src/Views/signin.vue
+++ src/Views/signin.vue
@@ -1,9 +1,15 @@
-<script setup>
-
-</script> 
+<script setup> 
+</script>
 
 <template>
-sign in
+<form action="http://localhost:3000/users" enctype="multipart/form-data" method="post">
+            <div class="form-group">
+                <input type="text" class="email" placeholder="email" name="email">
+                <input type="text" class="password" placeholder="password" name="password">
+                <input type="text" class="name" placeholder="name" name="name">
+                <input type="text" class="firstname" placeholder="prénom" name="firstname">
+            </div>
+        </form>
 </template>
 
 <style scoped>
