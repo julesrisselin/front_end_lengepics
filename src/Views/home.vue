def70c7c2ab537dec7aeb4457ed8d84a1e7d377c
Jules <julesrisselin@gmail.com>
2026-02-01 20:35:03 +0100

    début css

--- src/Views/home.vue
+++ src/Views/home.vue
@@ -11,40 +11,40 @@ async function getPicture() {
     const data = await resp.json();
     pictureInfos.value = data;
 
-    const respAccount = await fetch("http://localhost:3000/api/users/me",{
-        credentials : "include"
+    const respAccount = await fetch("http://localhost:3000/api/users/me", {
+        credentials: "include"
     })
-    
-    if (respAccount.status === 200){
+
+    if (respAccount.status === 200) {
         authentification.value = true;
     } else {
         authentification.value = false;
     }
 }
 
-    async function goToCurrentChallenge(){
-        router.push('/currentChallenge');
-    }
+async function goToCurrentChallenge() {
+    router.push('/currentChallenge');
+}
 
-    async function goToAccueil(){
-        router.push('/');
-    }
+async function goToAccueil() {
+    router.push('/');
+}
 
-    async function goToCurrentParticipations(){
-        router.push('/currentParticipations');
-    }
+async function goToCurrentParticipations() {
+    router.push('/currentParticipations');
+}
 
-    async function goToParticipations(){
-        router.push('/participations');
-    }
+async function goToParticipations() {
+    router.push('/participations');
+}
 
-    async function goToLogin(){
-        router.push('/login');
-    }
+async function goToLogin() {
+    router.push('/login');
+}
 
-    async function goToAccount(){
-        router.push('/account');
-    }
+async function goToAccount() {
+    router.push('/account');
+}
 
 
 
@@ -58,22 +58,27 @@ getPicture();
         <nav id="navbar">
             <ul>
                 <li>
-                    <button @click= goToAccueil()> Accueil </button>
+                    <button @click=goToAccueil()> Accueil </button>
                 </li>
                 <li>
-                    <button @click= goToCurrentChallenge()> Challenge </button>
+                    <button @click=goToCurrentChallenge()> Challenge </button>
                 </li>
-                <li>
-                    <button @click= goToCurrentParticipations()> Participations de la semaine </button>
+                <li id="dropnav">
+                    <button> Participations </button>
+                    <ul id="subnav">
+                        <li>
+                            <button @click=goToCurrentParticipations()> Participations de la semaine </button>
+                        </li>
+                        <li>
+                            <button @click=goToParticipations()> Toutes les participations </button>
+                        </li>
+                    </ul>
                 </li>
-                <li>
-                    <button @click= goToParticipations()> Toutes les participations </button>
+                <li v-if="!authentification" id="account">
+                    <button @click=goToLogin()> Connexion </button>
                 </li>
-                <li v-if="!authentification">
-                    <button @click= goToLogin() id="Account">  Connexion > </button>
-                </li>
-                <li v-else>
-                    <button @click= goToAccount() id="Account"> Mon compte > </button>
+                <li v-else id="account">
+                    <button @click=goToAccount()> Mon compte </button>
                 </li>
             </ul>
         </nav>
@@ -85,37 +90,60 @@ getPicture();
 
     <div id="first_scare">
         <div id="second_scare">
-            <img :src= "'http://localhost:3000/' +  pictureInfos.data.picture" ></img>
+            <img :src="'http://localhost:3000/' + pictureInfos.data.picture"></img>
         </div>
         <h2> {{ pictureInfos.data.title_theme }} </h2>
         <h4> {{ pictureInfos.data.description_theme }} </h4>
-        <button @click= goToCurrentChallenge()> Voir plus </button>
+        <button @click=goToCurrentChallenge()> Voir plus </button>
     </div>
 </template>
 
 <style scoped>
-
-#navbar{
+#navbar {
     background-color: #6B9080;
     border-radius: 45px;
     max-width: 100%;
     min-width: 100%;
-    max-height: 120px;
-    min-height: 10px;
+    max-height: 100%;
+    min-height: 100%;
+    position: relative;
+    display: block;
+}
 
-    
-    
+#navbar button {
+    background-color: #FFFFFF;
+    border-radius: 45px;
+    padding: 13px;
+    border-color: #FFFFFF;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 13px;
+
+}
+
+#dropnav {
+    position: relative;
+}
+
+#subnav {
+    display: none;
+    position: absolute;
+}
+
+.dropnav:hover.subnav {
+  display: block;
 }
 
 ul {
-  align-items: center;
-  display: flex;
-  list-style: none;
-  padding: 40px;
-  margin: 40px;
-  justify-content: space-between;
+    align-items: center;
+    display: flex;
+    list-style: none;
+    padding: 40px;
+    margin: 40px;
+    justify-content: baseline;
+    gap: 30px;
 }
 
-
-
+#account {
+    margin-left: 45%;
+}
 </style>
