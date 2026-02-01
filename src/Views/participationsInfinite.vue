dd81319f8da7bfb9c270b2265cb101c852ea2400
Jules <julesrisselin@gmail.com>
2026-02-01 11:50:39 +0100

    Filtre par Challenge ID et User ID

--- src/Views/participationsInfinite.vue
+++ src/Views/participationsInfinite.vue
@@ -4,9 +4,13 @@ import { useRouter } from 'vue-router'
 
 const participationsInfos = ref([]);
 const router = useRouter()
-const id_filter = ref("");
-const partInfos = ref([])
+const id_challenge = ref("");
+const user_id = ref("");
+const partInfosByChallenge = ref([]);
+const partInfosByUser = ref([]);
 const authentification = ref(false);
+const checkChallenge = ref(false);
+const checkUser = ref(false);
 
 async function getParticipations() {
     const resp = await fetch("http://localhost:3000/api/participations")
@@ -14,26 +18,46 @@ async function getParticipations() {
     participationsInfos.value = data;
 }
 
-async function getData() {
-
-    const id_challenge = id_filter;
-    console.log(id_challenge, id_filter)
-    const params = new URLSearchParams();
-    params.append("id_challenge", id_challenge);
-
-    const respPart = await fetch(`http://localhost:3000/api/participations?id_challenge=${params}`);
-    const dataPart = await respPart.json();
-    console.log(dataPart)
-    partInfos.value = dataPart;
-
-    const respAccount = await fetch("http://localhost:3000/api/users/me",{
-        credentials : "include"
+async function account() {
+    const respAccount = await fetch("http://localhost:3000/api/users/me", {
+        credentials: "include"
     })
-    if (respAccount.status === 200){
+    if (respAccount.status === 200) {
         authentification.value = true;
     } else {
         authentification.value = false;
     }
+}
+
+async function getPartByChallenge() {
+    const params = new URLSearchParams();
+    params.append("id_challenge", id_challenge.value);
+
+    const respPart = await fetch(`http://localhost:3000/api/participations?id_challenge=${params}`);
+    const dataPart = await respPart.json();
+    if (dataPart.data.length > 0) {
+        checkChallenge.value = true;
+    }
+    partInfosByChallenge.value = dataPart;
+}
+
+
+async function getPartByUserID() {
+
+    const params = new URLSearchParams();
+    params.append("user_id", user_id.value);
+
+    const respUser = await fetch(`http://localhost:3000/api/participations?user_id=${params}`);
+    const dataUser = await respUser.json();
+    if (dataUser.data.length > 0) {
+        checkUser.value = true;
+    }
+    partInfosByUser.value = dataUser;
+}
+
+async function showAll() {
+    checkChallenge.value = false
+    checkUser.value = false
 }
 
 async function goToCurrentChallenge() {
@@ -56,11 +80,11 @@ async function goToLogin() {
     router.push('/login');
 }
 
-async function goToAccount(){
-        router.push('/account');
-    }
+async function goToAccount() {
+    router.push('/account');
+}
 
-getData();
+account();
 getParticipations();
 </script>
 
@@ -81,10 +105,10 @@ getParticipations();
                     <button @click=goToParticipations()> Toutes les participations </button>
                 </li>
                 <li v-if="!authentification">
-                    <button @click= goToLogin() id="Account">  Connexion > </button>
+                    <button @click=goToLogin() id="Account"> Connexion > </button>
                 </li>
                 <li v-else>
-                    <button @click= goToAccount() id="Account"> Mon compte > </button>
+                    <button @click=goToAccount() id="Account"> Mon compte > </button>
                 </li>
             </ul>
         </nav>
@@ -92,8 +116,15 @@ getParticipations();
 
     <div id="" filter>
         <h2> Filtres </h2>
-        <input type="number" v-model="id_filter" placeholder="Entrez l'id du challenge" required /> <button
-            type="submit">Rechercher</button>
+        <input type="number" v-model="id_challenge" placeholder="Entrez l'id du challenge" required />
+        <button @click=getPartByChallenge() id=""> Afficher </button>
+
+        <br>
+
+        <input type="number" v-model="user_id" placeholder="Entrez l'id du user" required />
+        <button @click=getPartByUserID() id=""> Afficher </button>
+        <br>
+        <button @click=showAll() id=""> Tout Afficher </button>
 
 
 
@@ -103,7 +134,16 @@ getParticipations();
         <h2> Toutes les Participations ! </h2>
     </div>
 
-    <li v-for="(picture) in participationsInfos.data">
+    <li v-if="checkChallenge" v-for="(picture) in partInfosByChallenge.data">
+        <img :src="'http://localhost:3000/' + picture.picture_updated_url" id="picture"></img>
+        {{ picture.date_submission }}
+    </li>
+    <li v-else-if ="checkUser" v-for="(picture) in partInfosByUser.data">
+        <img :src="'http://localhost:3000/' + picture.picture_updated_url" id="picture"></img>
+        {{ picture.date_submission }}
+    </li>
+
+    <li v-else v-for="(picture) in participationsInfos.data">
         <img :src="'http://localhost:3000/' + picture.picture_updated_url" id="picture"></img>
         {{ picture.date_submission }}
     </li>
